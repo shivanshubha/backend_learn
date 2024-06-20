fcedeb37d25e7b17ba73e42cd9a466e6f43e42e1
shivanshubha <shivanshubharti655@gmail.com>
2024-06-20 17:37:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+// const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Your now the home page");
+});
+app.get("/twitter", (req, res) => {
+  res.send("Your now the twitter page");
+});
+app.get("/login", (req, res) => {
+  res.send("<h1>This is a login page</h1>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Your Port is now working ${process.env.PORT}`);
+});
